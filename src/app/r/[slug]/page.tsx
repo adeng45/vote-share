424d189dbc62923018ee6b8e0580e8abf1ef0f65
Anthony <andeng2@berkeley.edu>
2023-10-16 22:27:33 -0700

    subreddits

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import MiniCreatePost from '@/components/MiniCreatePost'
+import PostFeed from '@/components/PostFeed'
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 
-type Props = {}
+interface PageProps {
+  params: {
+    slug: string
+  }
+}
 
-export default function page({}: Props) {
+const page = async ({ params }: PageProps) => {
+  const { slug } = params
+
+  const session = await getAuthSession()
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        orderBy: {
+          createdAt: 'desc'
+        },
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+      },
+    },
+  })
+
+  if (!subreddit) return notFound()
+
   return (
-    <div>page</div>
+    <>
+      <h1 className='font-bold text-3xl md:text-4xl h-14'>
+        r/{subreddit.name}
+      </h1>
+      <MiniCreatePost session={session} />
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
+    </>
   )
 }
+
+export default page
